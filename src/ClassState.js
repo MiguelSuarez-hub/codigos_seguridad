037b09aa11a35a78f16ab556b9c2211cd32718cc
Mike <miguel.suarezf05@gmail.com>
2023-05-29 16:48:06 -0500

    added life cicle in class components

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Loading } from './Loading.js';
 
 class ClassState extends React.Component {
   constructor(props) {
@@ -6,16 +7,35 @@ class ClassState extends React.Component {
 
     this.state = {
       error: false,
+      loading: true
     };
   }
+
+  // UNSAFE_componentWillMount(){
+    // console.log('componentWillMount');
+  // }
+
+  // componentDidMount(){
+  //   console.log('componentDidMount');
+  // }
+
+  componentDidUpdate() {
+    if (!!this.state.loading) {
+      setTimeout(() =>{
+        this.setState({ loading: false });
+      },3000);
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Eliminar ClassState</h2>
         <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
         {this.state.error && (<p>Error: el codigo es incorrecto</p>)}
+        {this.state.loading && (<Loading/>)}
         <input placeholder='Código de seguridad' />
-        <button onClick={()=>this.setState(prevState => ({ error: !prevState.error}))}>Comprobar</button>
+        <button onClick={()=>this.setState(prevState => ({ loading: true }))}>Comprobar</button>
       </div>
     )
   }
