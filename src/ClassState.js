28a59d7e8477002d1a07e834bb7c3bfc2fbb31c2
Mike <miguel.suarezf05@gmail.com>
2023-05-29 18:02:10 -0500

    added functionality to compound states

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Loading } from './Loading.js';
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      value: '',
       error: false,
-      loading: true
+      loading: false
     };
   }
 
@@ -22,7 +25,12 @@ class ClassState extends React.Component {
   componentDidUpdate() {
     if (!!this.state.loading) {
       setTimeout(() =>{
-        this.setState({ loading: false });
+        if (this.state.value === SECURITY_CODE) {
+          this.setState({ loading: false})
+        } else {
+          this.setState({ error: true, loading: false });
+        }
+        
       },3000);
     }
   }
@@ -34,8 +42,8 @@ class ClassState extends React.Component {
         <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
         {this.state.error && (<p>Error: el codigo es incorrecto</p>)}
         {this.state.loading && (<Loading/>)}
-        <input placeholder='Código de seguridad' />
-        <button onClick={()=>this.setState(prevState => ({ loading: true }))}>Comprobar</button>
+        <input placeholder='Código de seguridad' value={this.state.value} onChange={(event) => {this.setState(prevState => ({ value: event.target.value }))}} />
+        <button onClick={()=>this.setState(prevState => ({ error: false, loading: true }))}>Comprobar</button>
       </div>
     )
   }
