8c741cd4f1570d8684680fc72c0be43a3e298e73
Mike <miguel.suarezf05@gmail.com>
2023-05-29 17:39:39 -0500

    added error state actualization

--- src/UseState.js
+++ src/UseState.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
+const SECURITY_CODE = 'paradigma';
+
 const UseState = () => {
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     if (!!loading) {
       setTimeout(() =>{
-        setLoading(false);
+        if(value === SECURITY_CODE){
+          setLoading(false);
+        } else {
+          setError(true);
+          setLoading(false);
+        }       
       },3000);
     }
   }, [loading])
@@ -18,9 +26,11 @@ const UseState = () => {
         <h2>Eliminar UseState</h2>
         <p>Por favor, escribe el código de seguridad para comprobar que quieres eliminar.</p>
         {error && (<p>Error: el codigo es incorrecto</p>)}
-        {loading && (<p>Cargando</p>)}
-        <input placeholder='Código de seguridad' />
-        <button onClick={()=>setLoading(true)}>Comprobar</button>
+        {loading && (<p>Cargando...</p>)}
+        <input placeholder='Código de seguridad' value={value} onChange={(event) => {
+          setValue(event.target.value)
+        }} />
+        <button onClick={()=>{setLoading(true); setError(false)}}>Comprobar</button>
       </div>
   )
 }
